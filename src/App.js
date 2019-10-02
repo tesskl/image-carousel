4b5aeef05157a23341241f147fef36fe5c488c40
Therese Kustvall Larsson <therese.kustvall.larsson@tactel.se>
2019-10-02 21:47:52 +0200

    Removing unnecessary files. Setting up base carousel canvas with dummy images.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import images from './images';
+import { Grid, IconButton } from "@material-ui/core";
+import { ArrowBack, ArrowForward } from "@material-ui/icons";
 
-function App() {
+export default function App() {
+  const imgStyle={
+    maxWidth: "600px",
+    maxHeight: "500px"
+  }
+  const containerStyle={
+    minHeight: '100vh'
+  }
+  const arrowStyle={
+    height: "2em",
+    width: "2em",
+  }
+
+ const index = 4;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={6} direction="row" justify="center" alignItems="center" style={containerStyle}>
+      <Grid item>
+        <IconButton>
+          <ArrowBack style={arrowStyle}/>
+        </IconButton>
+      </Grid>
+      {images.slice(index -1, index + 2).map(img => 
+      <Grid item>
+        <img key={img.id} src={img.url} style={imgStyle}/>
+      </Grid>)}
+      <Grid item>
+        <IconButton>
+          <ArrowForward style={arrowStyle}/>
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
 
-export default App;
